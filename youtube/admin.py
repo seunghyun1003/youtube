c484a7e880c7dd2135600a04bd02ab310e6f0e64
seunghyun1003 <s961003-@naver.com>
2021-01-26 00:55:53 +0900

    -

--- youtube/admin.py
+++ youtube/admin.py
@@ -1,8 +1,18 @@
 from django.contrib import admin
 
-from youtube.models import Video
+from youtube.models import Video,Comment
 
 @admin.register(Video)
 class PostAdmin(admin.ModelAdmin):
     list_display = ['id','title', 'file', 'des']
     list_display_links = ['title', 'file']
+
+    
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = (
+        'video', 
+        'comment_body',
+        'comment_date',
+    )
+    search_fields = ('video__title', 'comment_body', 'comment_date',)
